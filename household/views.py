d3c67d777f1ecc2345ccf622835dff43dbc5617b
I-yuichiro <52876660+I-yuichiro@users.noreply.github.com>
2020-04-06 06:55:52 +0900

    HAB

--- household/views.py
+++ household/views.py
@@ -5,16 +5,23 @@ from household import app
 def show_entries():
   return render_template('entries/index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route('/information')
+def information():
+  return render_template('entries/information.html')
+
+
+@app.route('/login', methods=['GET','POST'])
 def login():
-  if request.method == 'entries/index2.html':
+  if request.method == 'POST':
     if request.form['username'] != app.config['USERNAME']:
       print('ユーザー名が異なります')
     elif request.form['password'] != app.config['PASSWORD']:
       print('パスワードが異なります')
     else:
-      return redirect('/')
+      return redirect('entries/house.html')
   return render_template('entries/login.html')
+
 
 @app.route('/logout')
 def logout():
